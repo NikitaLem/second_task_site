bb8095670dab24ca48146271308e9dae84b6eee6
NikitaLem <saintsuigintoufag@gmail.com>
2018-03-19 00:58:09 +0600

    added header

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,4 @@
 import greet from './greeter.js';
-import '../blocks/buttons/buttons.js';
-import '../blocks/prc-charts/pie-charts.js';
-import '../blocks/prc-charts/prc-charts.js';
-import '../blocks/sliders/sliders.js';
-import '../blocks/stages/stages.js';
-import '../blocks/form-elements/form-elements.js';
-import '../blocks/calendar/calendar.js';
-import '../blocks/messaging/messaging.js';
 
 var html = require('../blocks/index.pug');
 const css = require('../blocks/index.styl');
