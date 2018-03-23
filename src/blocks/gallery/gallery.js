bec770d5daefeed5590f0a591956da9f6092083f
NikitaLem <saintsuigintoufag@gmail.com>
2018-03-24 04:03:55 +0600

    gallery fixed

--- src/blocks/gallery/gallery.js
+++ src/blocks/gallery/gallery.js
@@ -5,36 +5,20 @@ document.querySelector('.arw-btn_left').addEventListener('click', prev_img);
 
 function next_img() {
     var image = document.querySelectorAll('.image');
-    var text = document.querySelector('.gallery__text').innerText;
-    var altName = document.querySelector('.image_show').alt;
 	image[i].classList.remove('image_show');
 	i++;
 	if(i==image.length)
 		i=0;
     image[i].classList.add('image_show');
-    text = altName;
+    document.querySelector('.gallery__text').innerText = document.querySelector('.image_show').alt;;
 }
 
 function prev_img() {
     var image = document.querySelectorAll('.image');
-    var text = document.querySelector('.gallery__text').innerText;
-    var altName = document.querySelector('.image_show').alt;
 	image[i].classList.remove('image_show');
 	i--;
 	if(i<0)
 		i=image.length-1;
     image[i].classList.add('image_show');
-    console.log(altName);
-    console.log(text);
-    text = altName;
-    console.log(text);
+    document.querySelector('.gallery__text').innerText = document.querySelector('.image_show').alt;;
 }
-
-/*function setText() {
-    var text = document.querySelector('.gallery__text').innerText;
-    var altName = document.querySelector('.image_show').alt;
-    console.log(altName);
-    console.log(text);
-    text = altName;
-    console.log(text);
-}*/
