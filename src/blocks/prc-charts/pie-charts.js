37414848bb93621f294e8fc26ade9a317cfa53c8
NikitaLem <saintsuigintoufag@gmail.com>
2018-09-14 16:33:37 +0600

    keldim refactoring complete

--- src/blocks/prc-charts/pie-charts.js
+++ src/blocks/prc-charts/pie-charts.js
@@ -1,81 +1,39 @@
 import Chart from 'chart.js';
 
-Chart.defaults.global.legend.display = false;
+const myChartJs = function(idOfCanvas, dataForChart) {
+  Chart.defaults.global.legend.display = false;
 
-var keldimItems = document.querySelectorAll(".item__number_all");
-var keldimItemsValue = [];
+  const ctx = document.getElementById(idOfCanvas).getContext("2d");
 
-for(var i = 0; i < keldimItems.length; i++) {
-    keldimItemsValue[i] = keldimItems[i].value;
-}
-
-var ctx = document.getElementById("myChart").getContext("2d");
-
-var myChart = new Chart(ctx, {
+  const myChart = new Chart(ctx, {
     type: 'pie',
     data: {
-        labels: ["Веревки", "Карабины", "Жумары", "Корзинки", "Беседки", "Оттяжки"],
-        datasets: [{
-            data: keldimItemsValue,
-            backgroundColor: [
-                '#E49842',
-                '#F9F944',
-                '#23A296',
-                '#8560D5',
-                '#4AA737',
-                "#A04F4F"
-            ],
-            borderColor: [
-                '#E49842',
-                '#F9F944',
-                '#23A296',
-                '#8560D5',
-                '#4AA737',
-                "#A04F4F"
-            ],
-            borderWidth: 1
-        }]
+      labels: ["Веревки", "Карабины", "Жумары", "Корзинки", "Беседки", "Оттяжки"],
+      datasets: [{
+        data: dataForChart,
+        backgroundColor: [
+          '#E49842',
+          '#F9F944',
+          '#23A296',
+          '#8560D5',
+          '#4AA737',
+          "#A04F4F"
+        ],
+        borderColor: [
+          '#E49842',
+          '#F9F944',
+          '#23A296',
+          '#8560D5',
+          '#4AA737',
+          "#A04F4F"
+        ],
+        borderWidth: 1
+      }]
     },
     options: {
-        cutoutPercentage: 0
+      cutoutPercentage: 0
     }
-});
+  });
+};
 
-var cty = document.getElementById("myChartOne").getContext("2d");
-
-var keldimItemsCurrent = document.querySelectorAll(".item__number_current");
-var keldimItemsValueCurrent = [];
-
-for(var i = 0; i < keldimItemsCurrent.length; i++) {
-    keldimItemsValueCurrent[i] = keldimItemsCurrent[i].value;
-}
-
-var myChartOne = new Chart(cty, {
-    type: 'pie',
-    data: {
-        labels: ["Веревки", "Карабины", "Жумары", "Корзинки", "Беседки", "Оттяжки"],
-        datasets: [{
-            data: keldimItemsValueCurrent,
-            backgroundColor: [
-                '#E49842',
-                '#F9F944',
-                '#23A296',
-                '#8560D5',
-                '#4AA737',
-                "#A04F4F"
-            ],
-            borderColor: [
-                '#E49842',
-                '#F9F944',
-                '#23A296',
-                '#8560D5',
-                '#4AA737',
-                "#A04F4F"
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        cutoutPercentage: 0
-    }
-});
+export { myChartJs };
--- src/pages/keldim/keldim.js
+++ src/pages/keldim/keldim.js
@@ -1,6 +1,17 @@
-import '../../blocks/header/header.js';
-import '../../blocks/prc-charts/pie-charts.js';
+import '../../blocks/header/header';
+import { myChartJs } from '../../blocks/prc-charts/pie-charts';
 import '../../favicons/favicons';
 
 var html = require('./keldim.pug');
 const css = require('./keldim.styl');
+
+(function() {
+  const keldimItems = [...document.querySelectorAll('.storage-item__number_all')];
+  const keldimItemsCurrent = [...document.querySelectorAll('.storage-item__number_current')];
+
+  const keldimItemsValue = keldimItems.map(item => item.value);
+  const keldimItemsValueCurrent = keldimItemsCurrent.map(item => item.value);
+
+  myChartJs('myChartAll', keldimItemsValue);
+  myChartJs('myChartCurrent', keldimItemsValueCurrent);
+}());
